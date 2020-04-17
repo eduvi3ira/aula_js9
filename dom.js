3640d63785532b44482c3277a649808d00884bba
eduvi3ira <eduvi3ira@hotmail.com>
2020-04-16 22:02:02 -0300

    Commit

---
+++ dom.js
@@ -0,0 +1,14 @@
+function mensagem(){
+    alert("Botão Pressionado");
+}
+
+function escrever(){
+    document.write("Botão Pressionado");
+}
+
+function tabuada(){
+    var num = document.getElementById("numero").Value;
+    for(var x =0; x <= 10; x++){
+        document.whiteln( y * x + "<br/n");
+    }
+}
